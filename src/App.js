77a4a9a9bbb569cee1738564a83d062ed6dac7eb
Manthan Sharma <manthansharma.bhanwar@kanini.com>
2022-08-02 16:33:06 +0530

    homepage added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import './App.css';
-import Header from './components/register/header';
-import RegistrationForm from './components/register/registrationForm'
+import "./App.css";
+import Header from "./components/register/header";
+import RegistrationForm from "./components/register/registrationForm";
+import Home from "./components/home/home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    
     <div className="App">
-      <Header/>
-      <RegistrationForm/>
-    </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
 
+        <Route
+          path="/register"
+          element={
+            <>
+              <Header />
+              <RegistrationForm />
+            </>
+          }
+        />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/components/home/home.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+
+const linkStyle = {
+  margin: "5rem",
+  textDecoration: "none",
+};
+
+function Home() {
+  return (
+    <div>
+      <br></br>
+
+      <h1>Welcome to EazyBank!</h1>
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <div>
+        <Link to="/register" style={linkStyle}>
+          <button class="button-52" role="button">
+            Register
+          </button>
+        </Link>
+
+        <Link to="/login" style={linkStyle}>
+          <button class="button-52" role="button">
+            Login
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
+
+//end
